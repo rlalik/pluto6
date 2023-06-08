66fa58ca0723d637c7cf7d7b0d0989320a069d6c
Rafał Lalik <rafal.lalik@uj.edu.pl>
2023-06-08 13:51:50 +0200

    Strangeness plugin with new resonances and decay branches

--- plugins/strangeness/include/PStrangenessPlugin.h
+++ plugins/strangeness/include/PStrangenessPlugin.h
@@ -1,7 +1,9 @@
 // Author: I. Froehlich
 // Written: 12.10.2008
-// Revised: 
-// 
+// Revised:
+// Updates: Rafał Lalik, 05.06.2023 - New Y* decays channels
+//                                  - Software switches to modify decays
+//
 
 
 #ifndef _PSTRANGENESS_PLUGIN_H_
@@ -16,7 +18,7 @@
 using namespace std;
 
 class PStrangenessPlugin : public PDistributionCollection {
-    
+
  public:
 
     //constructor
@@ -24,12 +26,21 @@ class PStrangenessPlugin : public PDistributionCollection {
     //destructor
     ~PStrangenessPlugin();
 
-    Bool_t ExecCommand(const char *command, Double_t value); 
+    Bool_t ExecCommand(const char *command, Double_t value);
     Bool_t Activate(void);
 
+    static void EnableHadronDecays(Bool_t enabled) { hyperon_hadron_decays = enabled; }
+    static void EnablePhotonDecays(Bool_t enabled) { hyperon_photon_decays = enabled; }
+    static void EnableDalitzDecays(Bool_t enabled) { hyperon_dalitz_decays = enabled; }
+    static void EnableExperimentalDecays(Bool_t enabled) { hyperon_experimental_decays = enabled; }
  private:
 
     Int_t is_initialized;
+
+    static Bool_t hyperon_hadron_decays;
+    static Bool_t hyperon_photon_decays;
+    static Bool_t hyperon_dalitz_decays;
+    static Bool_t hyperon_experimental_decays;
 
     ClassDef(PStrangenessPlugin, 0) //Plugin to add new particles with strangeness and their decays
 };
